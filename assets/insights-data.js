9bc18e0ecc8eb40d0d08f647ecfd3d228ff63c2a
Vasilis Afentoulidis <billafent@gmail.com>
2025-10-29 19:01:58 +0200

    cursor

--- assets/insights-data.js
+++ assets/insights-data.js
@@ -1,5 +1,5 @@
 
 const INSIGHTS = [
-  { title: "Community programs reduce social isolation by ~20% within 6 months", tags:["Intervention","SDG 3"], sdg: 3, link:"/data.html#sources" },
-  { title: "Renewables adoption correlates with lower household energy burden", tags:["Energy","SDG 7"], sdg: 7, link:"/data.html#sources" }
+  { title: "Community programs reduce social isolation by ~20% within 6 months", tags:["Intervention","SDG 3"], sdgs: [3], link:"/data.html#sources" },
+  { title: "Renewables adoption correlates with lower household energy burden", tags:["Energy","SDG 7"], sdgs: [7], link:"/data.html#sources" }
 ];
