25817a53c56980647732cd6dcd9a2b79c3c5b8ed
Vasilis Afentoulidis <billafent@gmail.com>
2025-10-29 15:26:09 +0200

    n

--- assets/insights-data.js
+++ assets/insights-data.js
@@ -1,6 +1,5 @@
 
 const INSIGHTS = [
-  { title: "Income distribution database", tags:["Greece","Europe","SDG 1"], sdg: 1, link:"https://data-explorer.oecd.org/vis?tm=DF_IDD&pg=0&snb=1&vw=sr&df[ds]=dsDisseminateFinalDMZ&df[id]=DSD_WISE_IDD%40DF_IDD&df[ag]=OECD.WISE.INE&df[vs]=&pd=2010%2C&dq=.A.INC_DISP_GINI..._T.METH2012.D_CUR.&ly[rw]=REF_AREA%2CUNIT_MEASURE&ly[cl]=TIME_PERIOD&to[TIME_PERIOD]=false" },
   { title: "Community programs reduce social isolation by ~20% within 6 months", tags:["Intervention","SDG 3"], sdg: 3, link:"/data.html#sources" },
   { title: "Renewables adoption correlates with lower household energy burden", tags:["Energy","SDG 7"], sdg: 7, link:"/data.html#sources" }
 ];
